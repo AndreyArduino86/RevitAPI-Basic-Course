a1969059d94a5e0c42c5ee1b7f3f880ae18ac9f2
Андрей Козловский <andronkozmos86@gmail.com>
2024-02-14 00:05:08 +0300

    первая версия проекта

---
+++ RevitAPI Basic Course/Form1.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RevitAPI_Basic_Course
+{
+    [Transaction(TransactionMode.Manual)]
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        
+        private void CheckedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        
+        
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
